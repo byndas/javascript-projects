702ececaa8ce2e08ef8581d228fc10f50514ef27
Dan Volosnikov <byndasma@gmail.com>
2024-01-11 11:28:30 -0600

    conditionals studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,47 @@
 // Initialize Variables below
 
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+// let date = Monday 2019-03-18;
+// let time = 10:05; //:34;
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = 100; //%;
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
 
-// add logic below to verify all astronauts are ready
+// verify total number of astronauts does not exceed 7
+if (astronautCount <= 7) {
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
+    // verify all astronauts are ready
+    if (astronautStatus === "ready") {
 
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+        // verify total mass does not exceed 850000
+        if (totalMassKg <= 850000) {
 
-// add logic below to verify the fuel level is at 100%
+            // verify fuel temperature is within -150 and -300
+            if (fuelTempCelsius) {
 
-// add logic below to verify the weather status is clear
+                // verify the fuel level is at 100%
+                if (fuelLevel === 100) {
 
-// Verify shuttle launch can proceed based on above conditions
+                    // verify the weather status is clear
+                    if (weatherStatus === "clear") {
+
+                        // Verify shuttle launch can proceed based on above conditions
+                        if (preparedForLiftOff) {
+                                
+                        }
+                    }
+                }
+            }
+        }   
+    }
+}
