cfa59262057b5bada1549fdc8f5e9ab29a2db9cf
Dan Volosnikov <byndasma@gmail.com>
2024-01-14 12:46:13 -0600

    strings & arrays

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -3,44 +3,86 @@ let protoArray2 = "A;C;M;E";
 let protoArray3 = "space delimited string";
 let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
-strings = [protoArray1, protoArray2, protoArray3, protoArray4];
+let strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
 function reverseCommas() {
-	//TODO: 1. create and instantiate your variables.
-	let check;
+	// TODO: 1. create and instantiate your variables
+	let check = "";
 	let output;
-	//TODO: 2. write the code required for this step
+	//	TODO: 2. write the code required for this step
+	//	PSEUDO-CODE: If string uses commas to separate the words... protoArray4!
+	for (i=0; i<strings.length; i++) { // loop through strings array
+		if (strings[i].includes(",")) { // if item includes ","
+			// console.log(strings[i][0], !Number(strings[i][0]));
+			if (!Number(strings[i][0])) { // if first letter is not a number
+				check += strings[i]; // add the string to check
+			}
+		}
+	} //  console.log(check);
 
-	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
+	//	split into array & reverse items, join array into new comma-separated string
+	//	For example, "up,to,code,fun" becomes "fun,code,to,up"/
+	output = check.split(",").reverse().join();
+	//NOTE:  return your output as the final line in the function
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
+	let check = "";
 	let output;
-//TODO: write the code required for this step
-
-  
+// 	TODO: write the code required for this step
+//	If the string uses semicolons to separate the words
+	for (i=0; i<strings.length; i++) { // filter strings for ";"
+		if (strings[i].includes(";")) {
+			check += strings[i];
+		}
+	}
+	console.log(check);
+//	split it into an array & alphabetize the entries
+//	join the array into a new hyphen-separated string 
+//  For example, "up;to;code;fun" becomes "code-fun-to-up"
+    output = check.split(",").sort().join().replaceAll(";","-"); // "A-C-M-E"
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
+	let check = "";
 	let output;
-  //TODO: write the code required for this step
-
+//	TODO: write the code required for this step
+//	If the string uses spaces to separate the words
+	for (i=0; i<strings.length; i++) { // filter strings for ";"
+		if (strings[i].includes(" ")) {
+			if (!strings[i].includes(",")) { // filter out strings with ","
+				check += strings[i];
+			}
+		}
+	}
+	console.log(check);
+//	split into array & reverse alphabetize items
+//	join array into a new space-separated string
+	output = check.split(" ").sort().reverse().join(" ");
+//	For example, "to code up fun" becomes "up to fun code"
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
+	let check = "";
 	let output;
-	//TODO: write the code required for this step
-  
+	//	TODO: write the code required for this step
+	//	Consider: What if the string uses ‘comma spaces’ (, ) to separate the list?
+	for (i=0; i<strings.length; i++) { // filter strings for ";"
+		if (strings[i].includes(", ")) {
+			check += strings[i];
+		}
+	}
+	console.log(check);
+	//	Modify your code to produce the same result as part “b”
+	//  WHAT IS PART "B"?
+	//	Exclude extra spaces from final string
 	return output;
 }
 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,47 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
-//1) Use string methods to remove the first three characters from the string and add them to the end.
-//Hint - define another variable to hold the new string or reassign the new string to str.
+// 1) Use string methods to remove first 3 characters from string & add them to end.
+// Either: new variable = the new string
+// Or: reassign the new string to str
 
-//Use a template literal to print the original and modified string in a descriptive phrase.
+let arr = str.split(""); // convert str into array of letters
+console.log("arr:", arr);
 
-//2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let first3 = arr.splice(0,3); // store & remove first three letters from arr
+console.log("first3:", first3);
+console.log("arr:", arr);
 
-//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+arr = arr.concat(first3); // concat first3 to the end of arr
+console.log("arr concat first3:", arr);
+
+let newStr = arr.join(""); // convert arr into string & assign to newStr
+console.log("newStr:", newStr);
+
+// Use template literal to log the original and modified string in a descriptive phrase
+
+console.log(`The original string was ${str}\nThe modified string is ${newStr}`);
+
+
+// 2) Modify your code to accept user input
+//    Query the user to enter the number of letters that will be relocated
+
+let info = input.question(`How many letters to relocate? `);
+// console.log(info);
+
+
+// 3) Validate your code to deal with user inputs that are longer than the word
+//    In such cases, default to moving 3 characters
+//    Also, the template literal should note the error
+
+if (info > str.length) {
+    info = 3;
+    console.log(`ERROR: too many letters selected; defaults to three letters.`);
+} else {
+    let newArr = str.split(""); // store a new array of letters
+    let inputNum = newArr.splice(0, info); //  user input to splice newArr
+    // console.log("inputNum:", inputNum);
+
+    newStr = newArr.concat(inputNum).join(""); // convert newArr into string & reassign to newStr
+}
+console.log(`The original string was ${str}\nThe modified string is ${newStr}`);
