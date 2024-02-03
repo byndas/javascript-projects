65a81cb3fbdbb2fef3db7b059b4680d02c161a9e
Dan Volosnikov <byndasma@gmail.com>
2024-02-02 21:24:56 -0500

    February begins

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,18 @@
 // Import the modules exported from practiceExports.js below:
+const practice = require("./practiceExports.js");
+
+let arr = ["Hello", "World", 123, 987, "LC101"];
+
+console.log(practice.evenOrOdd(9));
+
+for (i = 0; i < 3; i++) {
+  console.log(practice.randomArrayElement(arr));
+}
+
+console.log(typeof practice.isPalindrome);
+console.log(practice.isPalindrome("mom"));
+console.log(practice.isPalindrome("that"));
+console.log(practice.isPalindrome("radar"));
+
+console.log(typeof practice);
+console.log(practice);
