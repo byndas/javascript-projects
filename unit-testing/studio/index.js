70d49149904be405f0cd727b61fa2c4c4ed5b1aa
Dan Volosnikov <byndasma@gmail.com>
2024-02-08 22:44:12 -0500

    unit test studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,35 @@
-
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+    let output = "";
+
+    if (num % 2 === 0) {
+      output += "Launch";
+    }
+
+    if (num % 3 === 0) {
+      output += "Code";
+    }
+
+    if (num % 5 === 0) {
+      if (output) {
+        output += "Rocks";
+      } else {
+        output += "Rocks";
+      }
+    }
+
+    if (!output) {
+      return "Rutabags! That doesn't work.";
+    } else if (output === "Launch Rocks") {
+      return `${output}! Smack!`;
+    } else {
+      return `${output}!`;
+    }
+  },
+};
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,43 @@
-// launchcode.test.js code:
-const launchcode = require('../index.js');
+const launchcode = require("../index.js");
 
-describe("Testing launchcode", function(){
+// Write a test checking for specific outcome & run to ensure it fails
+// Write a code snippet that passes the test
+// Repeat for remaining program features & refactor, Don’t Repeat Yourself
+describe("Testing launchcode", function () {
+  test("launchcode.organization is nonprofit", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  test("launchcode.executiveDirector is Jeff", () => {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("launchcode.percentageCoolEmployees is 100", () => {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("launchcode.programsOffered array values are Web Development, Data Analysis, Liftoff", () => {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+  });
 
-  // Write your unit tests here!
-  
+  test("num % 2 === 0", () => {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+  test("num % 3 === 0", () => {
+    expect(launchcode.launchOutput(9)).toBe("Code");
+  });
+  test("num % 5 === 0", () => {
+    expect(launchcode.launchOutput(25)).toBe("Rocks");
+  });
+  test("num % 2 === 0 && num % 3 === 0", () => {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks");
+  });
+  test("!output", () => {
+    expect(launchcode.launchOutput()).toBe("Rutabags! That doesn't work.");
+  });
+  test("output === Launch Rocks", () => {
+    expect(launchcode.launchOutput()).toBe(`${output}! Smack!`);
+  });
+  test("", () => {
+    expect(launchcode.launchOutput()).toBe(`${output}!`);
+  });
 });
