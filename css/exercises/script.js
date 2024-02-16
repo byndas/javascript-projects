7b580e8a21d7561281b94d92de21478a455342e1
Dan Volosnikov <byndasma@gmail.com>
2024-02-16 14:00:00 -0500

    css exercise

--- css/exercises/script.js
+++ css/exercises/script.js
@@ -1,1 +1,2 @@
-// You do not need to do anything with script.js right now! Later, we will learn how to add JavaScript to websites!
+// You do not need to do anything with script.js right now!
+// Later, we will learn how to add JavaScript to websites!
