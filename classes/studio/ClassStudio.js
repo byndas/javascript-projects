f20bf577cd81de9beecff091cf75ff08f18a246b
Dan Volosnikov <byndasma@gmail.com>
2024-02-15 21:02:58 -0500

    classes studio

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -5,42 +5,120 @@ class CrewCandidate {
     this.mass = mass;
     this.scores = scores;
   }
-  addingScores() {}
-  averagingScores() {}
-  candidateStatus() {}
+  addScore(newScore) {
+    this.scores.push(newScore);
+  }
+  averageScore() {
+    let scoreTotal = this.scores.reduce((total, nextScore) => {
+      return total + nextScore;
+    });
+    let result = scoreTotal / this.scores.length;
+    return Math.round(result * 10) / 10;
+  }
+  status(avgScore) {
+    if (avgScore >= 90) {
+      return "Accepted";
+    }
+    if (avgScore >= 80) {
+      return "Reserve";
+    }
+    if (avgScore >= 70) {
+      return "Probationary";
+    } else {
+      return "Rejected";
+    }
+  }
 }
-let bear = new CrewCandidate("Bubbba Bear", 135, [88, 85, 90]);
-let maltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
-let gator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
+let bubbaBear = new CrewCandidate("Bubbba Bear", 135, [88, 85, 90]);
+let merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+let gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
 
-// check that each candidate object correctly assigns keys & values
-console.log("bear", bear);
-console.log("maltese", maltese);
-console.log("gator", gator);
+// check each candidate object for correct keys & values
+console.log("bubbaBear:", bubbaBear);
+console.log("merryMaltese:", merryMaltese);
+console.log("gladGator:", gladGator);
 
-// Part 2 -- create candidate objects:
+// Part 1 -- create candidate class instance objects:
+
 // a. Bubba Bear has 135 kg mass & test scores: [88, 85, 90]
 // b. Merry Maltese 1.5 kg mass & test scores: [93, 88, 97]
 // c. Glad Gator has 225 kg mass & test scores: [75, 78, 62]
 
+// ---------------------------------------------------------
 // ||OUTSIDE of constructor||
 
-// create addingScores() method that pushes a new score parameter to this.scores array
+// Part 2 -- create pushScore() method that pushes newScore parameter to this.scores array
 // add 83 to Bubba’s record
-// console.log(candidateobjectName.scores)
-// -------------------------------------------------
-// Part 3 -- create averagingScores() method with no parameters
-// average = this.scores summation \ this.scores.length
-//          return the average rounded to one decimal place
-// console.log(Merry’s average test score) --> 92.7
+bubbaBear.addScore(83);
+console.log(`bubbaBear.scores:`, bubbaBear.scores);
 
-// create candidateStatus() method that, depending on perameter of candidate’s test score average,
-// returns one of these strings: 'Accepted', 'Reserve', 'Probationary', 'Rejected'
+// ------------------------------------------------------------
 
-// -- methods use ///this/// keyword to call other methods from inside a class
-// call candidateStatus() for all three candidates
-// consolelog(`${} earned an average test score of ${}% and has a status of ${}.`); --> use template literal
-// ---------------------------------------------------------------------------------------------------------
-// Part 4 - use methods to boost Glad Gator’s status to Reserve or higher
+// Part 3 -- create two more methods:
+
+// methods use ///this/// keyword to call other methods from inside a class
+
+// A. create averageScore() method with no parameters
+
+// let scoreTotal = this.scores.reduce((total, nextScore) => {
+//    return total + nextScore;
+// });
+
+// let result = scoreTotal / this.scores.length
+
+// return averageScore rounded to one decimal place
+// return Math.round(result * 10) / 10;
+
+console.log(`Merry’s average test score:`, merryMaltese.averageScore()); // --> 92.7
+
+// ================================================
+
+// B. create candidateStatus(avgScore) method that,
+//      depending on candidate’s average test score perameter, --> candaditeName.averageScore()
+//        returns one of these strings: 'Accepted', 'Reserve', 'Probationary', 'Rejected'
+//            if( avgScore >= 90 ) { return 'Accepted'; }
+//            if (avgScore >= 80) { return 'Reserve'; }
+//            if (avgScore >= 70) { return 'Probationary'; }
+//            else { return 'Rejected'; }
+
+//    call candidateStatus() for all three candidates:
+// bubbaBear.status(bubbaBear.averageScore);
+// merryMaltese.status(bubbaBear.averageScore);
+// gladGator.status(bubbaBear.averageScore);
+
+//  --> use template literal to:
+console.log(
+  `${
+    bubbaBear.name
+  } earned an average test score of ${bubbaBear.averageScore()}% and has a status of ${bubbaBear.status(
+    bubbaBear.averageScore()
+  )}.`
+);
+console.log(
+  `${
+    merryMaltese.name
+  } earned an average test score of ${merryMaltese.averageScore()}% and has a status of ${merryMaltese.status(
+    merryMaltese.averageScore()
+  )}.`
+);
+console.log(
+  `${
+    gladGator.name
+  } earned an average test score of ${gladGator.averageScore()}% and has a status of ${gladGator.status(
+    gladGator.averageScore()
+  )}.`
+);
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// Part 4 - use methods to boost gladGator.status to Reserve or higher
 //          how many tests to reach Reserve & Accepted status?
 //          scores cannot exceed 100% --> use a loop?
+
+console.log(
+  `${gladGator.name} will try until he reaches at least a score of 'Reserved'.`
+);
+// while (gladGator.status() !== "Accepted") {
+//   let testScore = Math.floor(Math.random() * (100 - 80 + 1) + 80);
+//   gladGator.addScore(testScore);
+// }
