2f6e976015f9e08b2b491a86a54c88db80e05f86
Dan Volosnikov <byndasma@gmail.com>
2024-02-18 01:34:10 -0500

    dom event exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,14 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
+  const missionAbort = document.getElementById("abortMission");
 
-    // Put your code for the exercises here.
-    
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+  missionAbort.addEventListener("mouseout", function (event) {
+    event.target.style.backgroundColor = "";
+  });
 }
 
 window.addEventListener("load", init);
