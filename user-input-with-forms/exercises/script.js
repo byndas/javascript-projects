ecced9e02f15dc1f264c7366a7276af9d9596918
Marc Byndas <byndasma@gmail.com>
2024-02-28 19:04:27 -0500

    forms exercise complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,24 @@
-//Code Your Solution Below
+// Handle submit event: an empty input prevents form submission
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+	let testName = document.querySelector("input[name=testName]");
+	let testDate = document.querySelector("input[name=testDate]");
+	let rocketName = document.querySelector("input[name=rocketName]");
+	let rocketBooster = document.querySelector("input[name=rocketBooster]");
+	let windRating = document.querySelector("input[name=windRating]");
+	let proServers = document.querySelector("input[name=proServers]");
+
+	if (
+		testName.value === "" ||
+		testDate.value === "" ||
+		rocketName.value === "" ||
+		rocketBooster.value === "" ||
+		windRating.value === "" ||
+		proServers.value === ""
+	) {
+		alert("All fields required!");
+		// prevent form submission
+		event.preventDefault();
+	}
+});
